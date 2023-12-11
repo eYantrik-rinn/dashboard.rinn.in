c2ea3c0504a73c0e3fb8fd5ad0d2bdf4603eb576
Anshul <tech@eyantrik.com>
2023-12-11 11:49:49 +0530

    some thing new

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,16 +2,16 @@
 	import { goto } from "$app/navigation";
 	import { page } from "$app/stores";
 	import { onMount } from "svelte";
-	// if (!$page.data.user) {
-	// 	onMount(async () => {
-	// 		goto("/login");
-	// 	});
-	// }
-	// if ($page.data.user) {
-	// 	onMount(async () => {
-	// 		goto(`/${$page.data.user.role}`);
-	// 	});
-	// }
+	if (!$page.data.user) {
+		onMount(async () => {
+			goto("/login");
+		});
+	}
+	if ($page.data.user) {
+		onMount(async () => {
+			goto(`/${$page.data.user.role}`);
+		});
+	}
 </script>
 
 <svelte:head>
