5952b5526659865abab7f8c89d211ddc2cceeecd
Anshul <tech@eyantrik.com>
2023-12-16 19:24:04 +0530

    new update

--- src/lib/components/brokerSidebar.svelte
+++ src/lib/components/brokerSidebar.svelte
@@ -1,12 +1,8 @@
-<script>
-</script>
 
 <svelte:head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <!-- Boxicons CSS -->
+    
     <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-    <title>Dashboard</title>
+   
 </svelte:head>
 
 <nav
--- src/routes/(dashboard)/(pages)/+layout.svelte
+++ src/routes/(dashboard)/(pages)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-    import BrokerSidebar from "$lib/components/BrokerSideBar.svelte";
+    // import BrokerSidebar from '$lib/components/brokerSidebar.svelte';
     import SideBar from "$lib/components/sideBar.svelte";
     import "../../../app.css";
     import Spinner from "$lib/components/spinner.svelte";
@@ -8,7 +8,7 @@
 
 <main>
     {#if $page.data.user.role === 'broker'}
-        <BrokerSidebar />
+        <!-- <BrokerSidebar /> -->
         <slot ></slot>
     {:else if $page.data.user.role === 'admin' }
     <SideBar />
