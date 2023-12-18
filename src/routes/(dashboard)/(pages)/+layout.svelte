4fc5d502d33bc306484edf60b55f71b0af78e217
Anshul <tech@eyantrik.com>
2023-12-18 11:37:48 +0530

    new update

--- src/routes/(dashboard)/(pages)/+layout.svelte
+++ src/routes/(dashboard)/(pages)/+layout.svelte
@@ -3,12 +3,13 @@
     import SideBar from "$lib/components/sideBar.svelte";
     import "../../../app.css";
     import Spinner from "$lib/components/spinner.svelte";
+    import BrokerSidebar from '$lib/components/brokerSidebar.svelte'
     import { page } from "$app/stores";
 </script>
 
 <main>
     {#if $page.data.user.role === 'broker'}
-        <!-- <BrokerSidebar /> -->
+        <BrokerSidebar />
         <slot ></slot>
     {:else if $page.data.user.role === 'admin' }
     <SideBar />
